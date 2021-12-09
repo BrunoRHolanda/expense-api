150c55e839cf5349b97dc0ec2dc6c6c3d28edbd1
Bruno Holanda <bholanda@grupoimagetech.com.br>
2021-12-09 10:35:33 -0400

    first commit

---
+++ src/test/java/br/com/itasystems/expense/ExpenseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.itasystems.expense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
